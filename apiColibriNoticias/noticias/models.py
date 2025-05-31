fcb0236d82d3c0a92861202caac148d7cc71d6fe
PedroLoriato <pedroeduardoloriato@gmail.com>
2025-05-31 08:24:29 -0300

    Atualizei um atributo do modelo de noticia Adicionei a modelagem do em parte Colibri Notícias, que contém a relação entre Notícias e Categorias feita em Django API

--- apiColibriNoticias/noticias/models.py
+++ apiColibriNoticias/noticias/models.py
@@ -10,7 +10,7 @@ class Noticia(models.Model):
     resumo = models.CharField(max_length=200)
     link = models.URLField(blank=False, null=False)
     dataHoraPublicacao = models.DateTimeField(blank=False, null=False)
-    dataHoraAdicao = models.DateTimeField(auto_now=True)
+    dataHoraAdicao = models.DateTimeField(auto_now_add=True)
     colaborador = models.CharField(max_length=200)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True, related_name='noticias')
 
